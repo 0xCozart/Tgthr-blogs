376f8f4f8a6a68480e4574e65395d7fdf266ab72
alan <alanvega002@gmail.com>
2020-09-24 22:02:07 -0700

    created post page with url id param

--- client/src/pages/post/[id].tsx
+++ client/src/pages/post/[id].tsx
@@ -1,4 +1,29 @@
 import React from "react";
 import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import urqlClient from "../../middleware/urqlClient";
+import { usePostQuery } from "../../generated/graphql";
+import Layout from "../../components/Layout";
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
+const Post = ({}) => {
+  const router = useRouter();
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+
+  if (fetching) {
+    return (
+      <Layout>
+        <div>loading...</div>
+      </Layout>
+    );
+  }
+  return <Layout>{data?.post?.text}</Layout>;
+};
+
+export default withUrqlClient(urqlClient, { ssr: true })(Post);
