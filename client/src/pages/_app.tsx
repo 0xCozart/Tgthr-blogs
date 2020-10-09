84a121bed425ec3a2286550f90d6a932a63da097
alan <alanvega002@gmail.com>
2020-10-08 17:00:24 -0700

    white theme enabled

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,12 +5,12 @@ import theme from "../theme";
 
 function MyApp({ Component, pageProps }: any) {
   return (
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider value="light">
+        <CSSReset />
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ThemeProvider>
   );
 }
 
